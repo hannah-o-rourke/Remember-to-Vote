2581f6e5b1593e2ee5f6857f0b48a8983b9508b1
hannah-o-rourke <42778538+hannah-o-rourke@users.noreply.github.com>
2026-03-13 16:32:59 +0000

    Configure next.config.js for GitHub Pages static export

--- next.config.js
+++ next.config.js
@@ -1,24 +1,11 @@
-module.exports = {
-  async headers() {
-    return [
-      {
-        // matching all API routes
-        source: '/api/:path*',
-        // destination: "https://react-polling-front-end-git-development-curtis-turk.vercel.app/:path*",
-        headers: [
-          { key: 'Access-Control-Allow-Credentials', value: 'true' },
-          {
-            key: 'Access-Control-Allow-Origin',
-            value: 'https://react-polling-front-end-git-development-curtis-turk.vercel.app',
-          },
-          { key: 'Access-Control-Allow-Methods', value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT' },
-          {
-            key: 'Access-Control-Allow-Headers',
-            value:
-              'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
-          },
-        ],
-      },
-    ];
-  },
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    output: 'export',
+    basePath: '/Remember-to-Vote',
+    assetPrefix: '/Remember-to-Vote/',
+    images: {
+          unoptimized: true,
+    },
 };
+
+module.exports = nextConfig;
