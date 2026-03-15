d641b44f56a4e2e95b501689260a0ebb21fb6c7a
hannah-o-rourke <42778538+hannah-o-rourke@users.noreply.github.com>
2026-03-15 15:00:12 +0000

    Update About.tsx

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -31,7 +31,7 @@ export default function About() {
       <hr id="half-break2" />
       <h4>What happens to my data?</h4>
       This site is part of a pilot to see if reminding people about the election increases voter
-      turnout. As soon as the pilot is evaluated, all data will be deleted.{' '}
+      turnout. As soon as the pilot is evaluated, all data will be deleted. By signing up, you agree to receive SMS voting reminders from Campaign Lab. We will use the mobile number you provide to send you SMS reminders to vote. We will only use your number for this purpose and for no other purpose. Our lawful basis is your consent. You can withdraw your consent at any time by contacting us at Noam@campaignlab.uk. We will keep your data only for as long as needed to provide this reminder service, and then securely delete it.{' '}
       {/* If you opt in to sharing your email, we will not delete it,
       but you will have the right to deletion in any further communications. */}
       <br />
